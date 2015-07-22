e2723f41132d265999e339be759cf5fc5035e05f
Rocca <rocca.peng@hunteron.com>
2015-07-22 16:09:04 +0800

    阻塞队列

---
+++ src/test/java/com/hunteron/hessian/BarrierHolder.java
@@ -0,0 +1,10 @@
+package com.hunteron.hessian;
+/*
+ * every producer and consumer must implement this interface
+ * Barrier is just a object lock for the synchronization between 
+ * producer and consumer
+ */
+public interface BarrierHolder {
+    Object getBarrier();
+}
+ 
