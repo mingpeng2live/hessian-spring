6d09628d9f069dc889c93b270a10982ce01c1cae
Rocca <rocca.peng@hunteron.com>
2015-07-29 10:55:06 +0800

    单消费者，多生产者，性能会好些

--- src/test/java/com/hunteron/hessian/Test.java
+++ src/test/java/com/hunteron/hessian/Test.java
@@ -2,7 +2,6 @@ package com.hunteron.hessian;
 
 import java.util.Map.Entry;
 import java.util.Random;
-import java.util.TimerTask;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -12,12 +11,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alibaba.druid.sql.dialect.oracle.ast.expr.OracleSizeExpr.Unit;
-
 public class Test {
 
 	private static final Logger logger = LoggerFactory.getLogger(Test.class);
 	private static final ConsumConcurrentHashMap<String, String> map = new ConsumConcurrentHashMap<>();
+	private static int i = 0;
+	private static int i2 = 0;
+	
 	
 	public static void main(String[] args) {
 		
@@ -30,6 +30,7 @@ public class Test {
 				long s = System.currentTimeMillis();
 				long e = 0l;
 				while((e = System.currentTimeMillis()) - s < 10 * 60 * 1000) {
+					i++;
 					map.put(UUID.randomUUID().toString(), RandomStringUtils.randomAlphanumeric(5));
 //					try {
 //						long nextLong = r.nextInt(2); // 该地方不能为1，如果为1，就没有睡眠效果
@@ -40,30 +41,31 @@ public class Test {
 				}
 			}
 		};
-//		Runnable runnable2 = new Runnable() {
-//			
-//			Random r = new Random(); 
-//			
-//			@Override
-//			public void run() {
-//				long s = System.currentTimeMillis();
-//				long e = 0l;
-//				while((e = System.currentTimeMillis()) - s < 10 * 60 * 1000) {
-//					map.put(UUID.randomUUID().toString(), RandomStringUtils.randomAlphanumeric(5));
-////					try {
-////						long nextLong = r.nextInt(2); // 该地方不能为1，如果为1，就没有睡眠效果
-////						Thread.sleep(nextLong);
-////					} catch (InterruptedException es) {
-////						es.printStackTrace();
-////					}
-//				}
-//			}
-//		};
+		Runnable runnable2 = new Runnable() {
+			
+			Random r = new Random(); 
+			
+			@Override
+			public void run() {
+				long s = System.currentTimeMillis();
+				long e = 0l;
+				while((e = System.currentTimeMillis()) - s < 10 * 60 * 1000) {
+					i2++;
+					map.put(UUID.randomUUID().toString(), RandomStringUtils.randomAlphanumeric(5));
+//					try {
+//						long nextLong = r.nextInt(2); // 该地方不能为1，如果为1，就没有睡眠效果
+//						Thread.sleep(nextLong);
+//					} catch (InterruptedException es) {
+//						es.printStackTrace();
+//					}
+				}
+			}
+		};
 		
 		Thread thread = new Thread(runnable);
 		thread.start();
-//		Thread thread2 = new Thread(runnable2);
-//		thread2.start();
+		Thread thread2 = new Thread(runnable2);
+		thread2.start();
 		
 		
 		map.regsiterRunnable(new Runnable() {
@@ -79,18 +81,18 @@ public class Test {
 			}
 		});
 		
-		map.regsiterRunnable(new Runnable() {
-			
-			private int i = 0;
-			
-			@Override
-			public void run() {
-				for (Entry<String, String> entry : map.entrySet()) {
-//					logger.info(i + "\t" + entry.getKey() + "\t" + entry.getValue() + "\t" + map.size());
-					map.remove(entry.getKey());
-				}
-			}
-		});
+//		map.regsiterRunnable(new Runnable() {
+//			
+//			private int i = 0;
+//			
+//			@Override
+//			public void run() {
+//				for (Entry<String, String> entry : map.entrySet()) {
+////					logger.info(i + "\t" + entry.getKey() + "\t" + entry.getValue() + "\t" + map.size());
+//					map.remove(entry.getKey());
+//				}
+//			}
+//		});
 		
 
 		ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
@@ -99,7 +101,7 @@ public class Test {
 			
 			@Override
 			public void run() {
-				logger.info("\t" + map.size());
+				logger.info(i+i2 + "\t" + map.size());
 			}
 			
 		}, 0, 10, TimeUnit.MILLISECONDS);
